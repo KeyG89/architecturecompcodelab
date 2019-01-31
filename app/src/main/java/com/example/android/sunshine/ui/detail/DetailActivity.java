1b7d483e38f5554296243ab295b35c73c93e3ed7
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-01 00:50:02 +0100

    12. Display the Data

--- app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
@@ -59,9 +59,15 @@ public class DetailActivity extends AppCompatActivity {
 
         mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
         long timestamp = getIntent().getLongExtra(WEATHER_ID_EXTRA, -1);
-        Date date = new Date(timestamp);
 
-        mViewModel = ViewModelProviders.of(this).get(DetailActivityViewModel.class);
+        Date date = SunshineDateUtils.getNormalizedUtcDateForToday();
+
+        DetailViewModelFactory  factory = InjectorUtils.provideDetailViewModelFactory(this.getApplicationContext(), date);
+        mViewModel = ViewModelProviders.of(this, factory).get(DetailActivityViewModel.class);
+
+        // mViewModel = ViewModelProviders.of(this).get(DetailActivityViewModel.class);
+//        DetailViewModelFactory factory = new DetailViewModelFactory(repository);
+
 
         // Observers changes in the WeatherEntry with the id mId
         mViewModel.getWeather().observe(this, weatherEntry -> {
@@ -70,27 +76,6 @@ public class DetailActivity extends AppCompatActivity {
         });
 
 
-        AppExecutors.getInstance().diskIO().execute(()-> {
-            try {
-
-                // Pretend this is the network loading data
-                Thread.sleep(4000);
-                Date today = SunshineDateUtils.getNormalizedUtcDateForToday();
-                WeatherEntry pretendWeatherFromDatabase = new WeatherEntry(1, 210, today,88.0,99.0,71,1030, 74, 5);
-                mViewModel.setWeather(pretendWeatherFromDatabase);
-
-                Thread.sleep(2000);
-                pretendWeatherFromDatabase = new WeatherEntry(1, 952, today,50.0,60.0,46,1044, 70, 100);
-                mViewModel.setWeather(pretendWeatherFromDatabase);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // THIS IS JUST TO RUN THE CODE; REPOSITORY SHOULD NEVER BE CREATED IN
-        // DETAILACTIVITY
-        // InjectorUtils.provideRepository(this).initializeData();
     }
 
     private void bindWeatherToUI(WeatherEntry weatherEntry) {
